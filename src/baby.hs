e5e79afbfe822b04e8248a58085d7b036b58888d
Sajit Kunnumkal <skunnumkal@amplify.com>
2014-12-19 16:44:30 -0500

    Weed

--- src/baby.hs
+++ src/baby.hs
@@ -1,1 +1,14 @@
 doubleMe x = 2 * x
+
+tripleMe y = y + y + y
+
+sqr a = a * a
+
+sumSquare x y = sqr x + sqr y + 2 * x * y
+
+--else is mandatory in haskell
+doubleSmallNumber n = if n < 100 then 2*n else n
+
+--aliasing
+foo x = bar x
+bar x = x + 1
