95d805fa6671eb7bb369428e52077536f6980f42
Sajit <sajit.kunnumkal@gmail.com>
2015-04-11 19:51:36 -0400

    problem 2

--- src/99questions/problem1.hs
+++ src/99questions/problem1.hs
@@ -1,4 +1,6 @@
 --(*) Find the last element of a list. returns Nothing if empty list
 -- things to learn, the Haskell Maybe Just Nothing paradigm
 
+module Problem1(last_el) where
+
 last_el aList = if(length(aList) > 0) then Just (aList !! (length(aList)-1)) else Nothing
--- src/99questions/problem2.hs
+++ src/99questions/problem2.hs
@@ -1,1 +1,7 @@
 --problem2
+--(*) Find the last but one element of a list.
+module Problem2(lastb1_el) where
+
+--import Problem1
+
+lastb1_el aList = if(length(aList) >= 2) then Just (aList !! (length(aList)-2)) else Nothing
