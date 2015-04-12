03d8179f61737b235fdbbf098efcb864de77944a
Sajit <sajit.kunnumkal@gmail.com>
2015-04-11 23:00:51 -0400

    Problem 3

--- src/99questions/problem2.hs
+++ src/99questions/problem2.hs
@@ -5,3 +5,12 @@ module Problem2(lastb1_el) where
 --import Problem1
 
 lastb1_el aList = if(length(aList) >= 2) then Just (aList !! (length(aList)-2)) else Nothing
+
+-- problem3 
+--Find the K'th element of a list. 
+
+kth_el aList  k = if(length(aList) < k) then Nothing else Just (aList !! (k-1))
+
+-- problem 4 
+-- Find the number of elements in a list
+num_elements aList = length(aList)
