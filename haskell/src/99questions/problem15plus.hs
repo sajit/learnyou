1b68da7f69c1fb8013ada8f718b40e1cb8225e21
Sajit Mathew Kunnumkal <sajit.kunnumkal@gmail.com>
2015-04-28 18:31:18 -0400

    ard

--- haskell/src/99questions/problem15plus.hs
+++ haskell/src/99questions/problem15plus.hs
@@ -3,3 +3,12 @@
 
 replicate_me [] n = []
 replicate_me (x:xs) n = [x | y <- [1..n]] ++ replicate_me xs n
+
+--Problem 16
+--(**) Drop every N'th element from a list.
+
+dropNth [] n acc = acc
+dropNth x n acc 
+ | (length x) < n = x ++ acc
+ | (length x) == n = (take (n-1) x) ++ acc
+ | otherwise =  dropNth (drop n x) n ((take (n-1) x) ++ acc)  
