c5bd81496f280be8a18bdc157af613d56f9ff8d4
Sajit <sajit.kunnumkal@gmail.com>
2015-05-01 23:49:55 -0400

    tail.rec methods

---
+++ scala/minimal-scala/src/main/scala/fpscala/chapter2/Chapter2.scala
@@ -0,0 +1,39 @@
+package fpscala.chapter2
+
+/**
+ * Created by sajit on 5/1/15.
+ */
+object Chapter2 {
+
+  //@annotation.tailrec
+  /**
+   * This is not a tailrec function as we are doing some computation. Hence compiler will throw an error
+   * @param n
+   * @return
+   */
+  def factorial(n:Int):Int = {
+    n match {
+      case 0 => 1
+      case 1 => 1
+      case _ => n * factorial(n-1)
+    }
+  }
+
+  /**
+   * tail_factorial is not a recursive call. So no sense in making tail rec
+   * @param n
+   * @return
+   */
+  def tail_factorial(n:Int):Int = {
+    @annotation.tailrec
+    def do_tail_factorial(n:Int,acc:Int):Int = {
+      n match {
+        case 0 => acc
+        case 1 => acc
+        case _ => do_tail_factorial(n-1,n*acc)
+      }
+    }
+    do_tail_factorial(n,1)
+  }
+
+}
--- scala/minimal-scala/src/test/scala/HelloSpec.scala
+++ scala/minimal-scala/src/test/scala/HelloSpec.scala
@@ -1,8 +1,14 @@
+import fpscala.chapter2.Chapter2
 import org.scalatest._
 import org.scalatest.matchers.ShouldMatchers
 
 class HelloSpec extends FlatSpec with ShouldMatchers {
   "Hello" should "have tests" in {
     true should be === true
+  }
+
+  "Chapter 2 exercises" should "work in" in {
+    Chapter2.factorial(3) should be === 6
+    Chapter2.tail_factorial(4) should be === 24
   }
 }
