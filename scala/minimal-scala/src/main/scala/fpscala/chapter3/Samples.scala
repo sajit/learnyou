9080d1541ea0c0f9a943bb509568ab522d77a0dc
Sajit <sajit.kunnumkal@gmail.com>
2015-09-09 22:58:35 -0400

    Shadys back

--- scala/minimal-scala/src/main/scala/fpscala/chapter3/Samples.scala
+++ scala/minimal-scala/src/main/scala/fpscala/chapter3/Samples.scala
@@ -3,51 +3,49 @@ package fpscala.chapter3
 /**
  * Created by sajit on 5/14/15.
  */
-object Samples {
 
-}
-sealed trait List[+A]
-case object Nil extends List[Nothing]
-case class Cons[+A](head:A,tail:List[A]) extends List[A]
+sealed trait BList[+A]
+case object BNil extends BList[Nothing]
+case class BCons[+A](head:A,tail:BList[A]) extends BList[A]
 
-object List{
-  def sum(ints:List[Int]):Int = ints match {
-    case Nil => 0
-    case Cons(x,xs) => x + sum(xs)
+object BList{
+  def sum(ints:BList[Int]):Int = ints match {
+    case BNil => 0
+    case BCons(x,xs) => x + sum(xs)
   }
 
-  def product(ints:List[Int]):Int = ints match {
-    case Nil => 1
-    case Cons(x,xs) => x * product(xs)
+  def product(ints:BList[Int]):Int = ints match {
+    case BNil => 1
+    case BCons(x,xs) => x * product(xs)
   }
 
-  def tail[A](l: List[A]): List[A] =
+  def tail[A](l: BList[A]): BList[A] =
     l match {
-      case Nil => sys.error("tail of empty list")
-      case Cons(_,t) => t
+      case BNil => sys.error("tail of empty list")
+      case BCons(_,t) => t
     }
-  def setHead[A](l: List[A],newHead:A): List[A] =
+  def setHead[A](l: BList[A],newHead:A): BList[A] =
     l match {
-      case Nil => Cons(newHead,Nil)
-      case Cons(_,t) => Cons(newHead,t)
+      case BNil => BCons(newHead,BNil)
+      case BCons(_,t) => BCons(newHead,t)
     }
 
-  def drop[A](l: List[A],n:Int): List[A] =
+  def drop[A](l: BList[A],n:Int): BList[A] =
     l match {
-      case Nil => sys.error("drop on empty list")
-      case Cons(h,t) => n match {
+      case BNil => sys.error("drop on empty list")
+      case BCons(h,t) => n match {
         case x if x<0 => sys.error("bad argument")
-        case 0 => Cons(h,t)
+        case 0 => BCons(h,t)
         case x if x>0 => drop(t,n-1)
       }
     }
 
-  def dropWhile[A](l: List[A],predicate: A => Boolean): List[A] =
+  def dropWhile[A](l: BList[A],predicate: A => Boolean): BList[A] =
     l match {
-      case Nil => Nil
-      case Cons(h,t) => {
+      case BNil => BNil
+      case BCons(h,t) => {
         if(predicate(h)){
-          Cons(h,dropWhile(t,predicate))
+          BCons(h,dropWhile(t,predicate))
         }
         else{
           dropWhile(t,predicate)
@@ -55,62 +53,62 @@ object List{
       }
     }
 
-  def dropWhile2[A](l: List[A],predicate: A => Boolean): List[A] =
+  def dropWhile2[A](l: BList[A],predicate: A => Boolean): BList[A] =
     l match {
-      case Nil => Nil
-      case Cons(h,t) => predicate(h) match {
-        case true => Cons(h,dropWhile2(t,predicate))
+      case BNil => BNil
+      case BCons(h,t) => predicate(h) match {
+        case true => BCons(h,dropWhile2(t,predicate))
         case false => dropWhile2(t,predicate)
       }
     }
 
-  def append[A](a1:List[A],a2:List[A]):List[A] = (a1,a2) match {
-    case (Nil,a2) => a2
-    case (Cons(h,t),a2) => Cons(h,append(t,a2))
+  def append[A](a1:BList[A],a2:BList[A]):BList[A] = (a1,a2) match {
+    case (BNil,a2) => a2
+    case (BCons(h,t),a2) => BCons(h,append(t,a2))
   }
 
-  def init[A](al:List[A]):List[A] = al match {
-    case Nil => sys.error("impossible")
-    case Cons(h,Nil) => Nil
-    case Cons(h,t) => Cons(h,init(t))
+  def init[A](al:BList[A]):BList[A] = al match {
+    case BNil => sys.error("impossible")
+    case BCons(h,BNil) => BNil
+    case BCons(h,t) => BCons(h,init(t))
   }
 
-  def last[A](al:List[A]):A = al match {
-    case Nil => sys.error("nooo")
-    case Cons(x,Nil) => x
-    case Cons(h,t) => last(t)
+  def last[A](al:BList[A]):A = al match {
+    case BNil => sys.error("nooo")
+    case BCons(x,BNil) => x
+    case BCons(h,t) => last(t)
   }
 
-  def foldRight[A,B] (as:List[A],z:B)(f:(A,B) => B):B = as match {
-    case Nil => z
-    case Cons(x,xs) => f(x,foldRight(xs,z)(f))
+  def foldRight[A,B] (as:BList[A],z:B)(f:(A,B) => B):B = as match {
+    case BNil => z
+    case BCons(x,xs) => f(x,foldRight(xs,z)(f))
   }
 
-  def length[A](as:List[A]):Int = foldRight(as,0)((_,acc) => acc + 1)
+  def length[A](as:BList[A]):Int = foldRight(as,0)((_,acc) => acc + 1)
 
 
   @annotation.tailrec
-  def mfl[A,B](as:List[A],z:B)(f:(B,A) => B): B = as match{
-    case Nil => z
+  def mfl[A,B](as:BList[A],z:B)(f:(B,A) => B): B = as match{
+    case BNil => z
     case _ => {
       mfl(init(as),f(z,last(as)))(f)
 
     }
   }
 
-  def sumIsh(as:List[Int]):Int = mfl(as,0)((a,b) => a + b)
+  def sumIsh(as:BList[Int]):Int = mfl(as,0)((a,b) => a + b)
 
-  def productIs(as:List[Int]) = mfl(as,1)((a,b) => a * b)
+  def productIs(as:BList[Int]) = mfl(as,1)((a,b) => a * b)
 
-  def map[A,B] (as:List[A])(f: A => B):List[B] = as match {
-    case Nil => Nil
-    case Cons(h,t) => Cons(f(h),map(t)(f))
+  def map[A,B] (as:BList[A])(f: A => B):BList[B] = as match {
+    case BNil => BNil
+    case BCons(h,t) => BCons(f(h),map(t)(f))
   }
 
-  def filter[A](as:List[A],f:A => Boolean):List[A] = as match {
-    case Nil => Nil
-    case Cons(h,t) => f(h) match {
-      case true => Cons(h,filter(t,f))
+  def filter[A](as:BList[A],f:A => Boolean):BList[A] = as match {
+    case BNil => BNil
+    case BCons(h,t) => f(h) match {
+      case true => BCons(h,filter(t,f))
       case false => filter(t,f)
     }
   }
@@ -121,5 +119,5 @@ object List{
    * @tparam A
    * @return
    */
-  def apply[A](as:A*):List[A]= if(as.isEmpty) Nil else Cons(as.head,apply(as.tail: _*))
+  def apply[A](as:A*):BList[A]= if(as.isEmpty) BNil else BCons(as.head,apply(as.tail: _*))
 }
---
+++ scala/minimal-scala/src/test/scala/fpscala/chapter3/Chapter3Spec.scala
@@ -0,0 +1,19 @@
+package fpscala.chapter3
+
+import org.scalatest.{Matchers, FlatSpec}
+
+
+/**
+ * Created by sajit on 9/9/15.
+ */
+class Chapter3Spec extends FlatSpec with Matchers {
+
+
+    "foldright " should " apply a function " in {
+      val bList = BCons(4,BCons(5,BCons(3,BCons(2,BNil))))
+      val sum = BList.foldRight(bList,0)((x,y) => x + y)
+      sum should be === (14)
+    }
+
+
+}
