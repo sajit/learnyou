e82904b1159d52bcd7e14c2e9caa3d8ae7da0279
Sajit <sajit.kunnumkal@gmail.com>
2015-09-19 08:38:47 -0400

    Chapter 4. init

---
+++ scala/minimal-scala/src/main/scala/fpscala/chapter3/BOption.scala
@@ -0,0 +1,13 @@
+package fpscala.chapter3
+
+/**
+ * 	Meaning	Scala notation
+covariant	C[T’] is a subclass of C[T]	[+T]
+contravariant	C[T] is a subclass of C[T’]	[-T]
+invariant	C[T] and C[T’] are not related	[T]
+
+ * Created by sajit on 9/19/15.
+ */
+sealed trait BOption[+A]
+case class BSome[+A](get:A) extends BOption[A]
+case object BNone extends BOption[Nothing]
