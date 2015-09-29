354d4bfa5424043d3856e5a64b92a28f73be5e58
Sajit <sajit.kunnumkal@gmail.com>
2015-09-28 23:20:49 -0400

    Chapter 4 Either type

--- scala/minimal-scala/src/main/scala/fpscala/chapter4/Beither.scala
+++ scala/minimal-scala/src/main/scala/fpscala/chapter4/Beither.scala
@@ -4,6 +4,19 @@ package fpscala.chapter4
  * Created by sajit on 9/28/15.
  */
 
-sealed trait Beither[+E,+A]
+sealed trait Beither[+E,+A] {
+  //from the solutions :(
+  //https://github.com/fpinscala/fpinscala/blob/master/answers/src/main/scala/fpinscala/errorhandling/Either.scala
+  def map[B](f: A => B): Beither[E,B] = this match {
+    case Right(a) => Right(f(a))
+    case Left(e) => Left(e)
+  }
+
+  def flatMap[EE >:E, B] (f: A => Beither[EE,B]):Beither[EE,B] = this match {
+    case Right(a) => f(a)
+    case Left(e) => Left(e)
+  }
+}
 case class Left[+E](value:E) extends Beither[E, Nothing]
 case class Right[+A](value:A) extends Beither[Nothing,A]
+
