df11712ffcaf2563c1d45eb9bd5d182487f0e159
Sajit <sajit.kunnumkal@gmail.com>
2015-12-04 23:00:17 -0500

    Chapter 7. buildup

--- scala/minimal-scala/src/main/scala/fpscala/chapter7/Par.scala
+++ scala/minimal-scala/src/main/scala/fpscala/chapter7/Par.scala
@@ -1,6 +1,8 @@
 package fpscala.chapter7
 
-import java.util.concurrent.{ExecutorService, Future}
+
+import java.util.concurrent._
+
 
 /**
  * Created by sajit on 12/2/15.
@@ -10,5 +12,18 @@ object Par {
   type Par[A] = ExecutorService => Future[A]
 
   def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
+
+  def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
+
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    def isDone = true
+
+    def isCancelled = false
+
+    def get(timeout: Long, units: TimeUnit) = get
+
+    def cancel(eventIfRunning: Boolean) = false
+
+  }
 
 }
