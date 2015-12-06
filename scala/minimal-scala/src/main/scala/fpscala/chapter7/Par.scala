9fc74a5cc3b10be366c1de2b83fc55fbdb648838
Sajit <sajit.kunnumkal@gmail.com>
2015-12-06 10:50:51 -0500

    Chapter 7, par, map function

--- scala/minimal-scala/src/main/scala/fpscala/chapter7/Par.scala
+++ scala/minimal-scala/src/main/scala/fpscala/chapter7/Par.scala
@@ -16,12 +16,6 @@ object Par {
 
   def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
 
-  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = es => {
-    val af = a(es)
-    val bf = b(es)
-    UnitFuture(f(af.get(), bf.get()))
-  }
-
   def map3[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = es => {
     val af: Future[A] = a(es)
     val bf: Future[B] = b(es)
@@ -34,15 +28,24 @@ object Par {
     }
   }
 
+  def map[A, B](pa: Par[A])(f: A => B): Par[B] = map2(pa, unit(()))((a, _) => f(a))
+
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = es => {
+    val af = a(es)
+    val bf = b(es)
+    UnitFuture(f(af.get(), bf.get()))
+  }
+
+  def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
+
   def asyncF[A, B](f: A => B): A => Par[B] = { a => lazyUnit(f(a)) }
 
   def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
 
-  def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
-
   def fork[A](a: => Par[A]): Par[A] = es => es.submit(new Callable[A] {
     override def call(): A = a(es).get
   })
+
 
   private case class UnitFuture[A](get: A) extends Future[A] {
     def isDone = true
