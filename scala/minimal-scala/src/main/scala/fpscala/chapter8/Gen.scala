1338c5ddc282b15613c608444413db4a89c26d3f
sajit <sajit.kunnumkal@gmail.com>
2016-03-07 00:05:56 -0500

    Not sure Im doing

--- scala/minimal-scala/src/main/scala/fpscala/chapter8/Gen.scala
+++ scala/minimal-scala/src/main/scala/fpscala/chapter8/Gen.scala
@@ -1,10 +1,20 @@
 package fpscala.chapter8
 
-import fpscala.chapter6.CandyShop.State
+
+
 import fpscala.chapter6.RNG
+import fpscala.chapter6.PreludeRNGs
 
 
 case class Gen[A](sample: State[RNG, A])
 object Gen {
-  def choose(start:Int, stopExclusive:Int):Gen[Int] = ???
+ 
+    /* We could write this as an explicit state action, but this is far less
+   convenient, since it requires us to manually thread the `RNG` through the
+   computation. */
+  def choose2(start: Int, stopExclusive: Int): Gen[Int] = 
+      Gen(State(rng => PreludeRNGs.nonNegativeInt(rng) match {
+    case (n,rng2) => (start + n % (stopExclusive-start), rng2)
+  }))
+    
 }
