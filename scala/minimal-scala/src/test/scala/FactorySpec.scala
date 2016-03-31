8397546f50f2a4a77a373870e798764274bffdb5
sajit <sajit.kunnumkal@gmail.com>
2016-03-31 06:17:36 -0400

    Illustrate an example of function as case class param

--- scala/minimal-scala/src/test/scala/FactorySpec.scala
+++ scala/minimal-scala/src/test/scala/FactorySpec.scala
@@ -2,9 +2,15 @@ import com.example.FactoryBoy
 import org.scalatest._
 class FactorySpec extends FlatSpec with Matchers {
   "FactoryBoy" should "calc squares " in {
-    val f:(Int => Int) = {x => x*x}
-    val sqFactory = FactoryBoy(f)
+    val squares:(Int => Int) = {x => x*x}
+    val sqFactory = FactoryBoy(squares)
     sqFactory.calc(3) should be (9)
+    
+  }
+  "FactoryBoy" should "calc cubes " in {
+    val cubes:(Int => Int) = {x => x*x*x}
+    val cbFactory = FactoryBoy(cubes)
+    cbFactory.calc(3) should be (27)
     
   }
   
