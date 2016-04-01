b4ba418b70b97475330df63add3fc948d2ca25fc
sajit <sajit.kunnumkal@gmail.com>
2016-04-01 08:30:17 -0400

    More practice exercises.

--- scala/minimal-scala/src/test/scala/fpscala/chapter8/StateSpec.scala
+++ scala/minimal-scala/src/test/scala/fpscala/chapter8/StateSpec.scala
@@ -2,15 +2,32 @@ package fpscala.chapter8
 
 import fpscala.BaseSpec
 class StateSpec extends BaseSpec{
-  it should "map a state " in {
-    val simpleRng = RNG.Simple(10l)
-    //val runner:(RNG => (String,RNG)) =  ??? 
+  val simpleRng = RNG.Simple(10l)
+  
+  it should "map a state " in {  
+    //convert a string to a state[string]
     val s1:State[RNG,String] = State.unit("hello")
     val s2: State[RNG,Int] = s1.map { x => x.length() }
     val (result,nextRNG):(Int,RNG) = s2.run(simpleRng)
     result should be (5)
     
     
+  }
+  
+  it should "combine two states" in {
+    val s1:State[RNG,String] = State.unit("hello")
+    val s2: State[RNG,String] = State.unit("world") 
+    val s3:State[RNG,String] = s1.map2(s2)((a,b) => a + b)
+    val (result,nextRNG):(String,RNG) = s3.run(simpleRng)
+    result should be ("helloworld")
+  }
+  
+  it should "flatMap to learn" in {
+    val s1:State[RNG,String] = State.unit("hello")
+    val flatMapper:(String => State[RNG,String]) = { aString => State.unit(aString.toUpperCase()) } 
+    val s2:State[RNG,String] = s1.flatMap(flatMapper)
+    val (result,nextRNG):(String,RNG) = s2.run(simpleRng)
+    result should be ("HELLO")
   }
   
 }
