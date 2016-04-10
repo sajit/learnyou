0181232220c5a6ef2bced94a08f925a78f671535
sajit <sajit.kunnumkal@gmail.com>
2016-04-10 11:10:38 -0400

    Setup work for section 8.2.5

--- scala/minimal-scala/src/main/scala/fpscala/chapter8/Prop.scala
+++ scala/minimal-scala/src/main/scala/fpscala/chapter8/Prop.scala
@@ -1,19 +1,34 @@
 package fpscala.chapter8
 
-//object Prop{
-//  type FailedCase = String
-//  type SuccessCount = Int
-//}
+
+
 trait Prop {
-  type FailedCase = String
-  type SuccessCount = Int
+  
  
   def check:Boolean
   
-  def betterCheck: Either[(FailedCase,SuccessCount),SuccessCount] = ???
+  def betterCheck: Either[(Prop.FailedCase,Prop.SuccessCount),Prop.SuccessCount] = ???
   
   //copied from answer 3 chapter 8
     def &&(p: Prop): Prop = new Prop {
        def check = Prop.this.check && p.check
     }
+}
+
+object Prop {
+  type FailedCase = String
+  type SuccessCount = Int
+  
+  sealed trait Result {
+    def isFalsified :Boolean
+  }
+  case object Passed extends Result {
+    def isFalsified = false
+  }
+  case class Falsified(failure:FailedCase,successes:SuccessCount) extends Result {
+    def isFalsified = true
+  }
+  
+  def forAll[A](a:Gen[A])(f:A=> Boolean):Prop = ???
+
 }
