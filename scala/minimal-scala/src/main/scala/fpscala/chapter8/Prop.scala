c58f94189cf996fee15f8423a14b5c5093180a4f
sajit <sajit.kunnumkal@gmail.com>
2016-04-12 16:43:39 -0400

    Listing 8.3

--- scala/minimal-scala/src/main/scala/fpscala/chapter8/Prop.scala
+++ scala/minimal-scala/src/main/scala/fpscala/chapter8/Prop.scala
@@ -1,5 +1,7 @@
 package fpscala.chapter8
 
+import fpscala.chapter5._
+
 
 
 
@@ -22,6 +24,25 @@ object Prop {
     def isFalsified = true
   }
   
-  def forAll[A](a:Gen[A])(f:A=> Boolean):Prop = ???
+  def randomStream[A](g:Gen[A])(rng:RNG):Stream[A] = 
+    StreamUtils. //Generates an infinite stream of A values by repeatedly sampling in generator
+    unfold(rng)(rng => Some(g.sample.run(rng)))
+    
+  def buildMsg[A](s: A, e: Exception): String =
+    s"test case: $s\n" +
+    s"generated an exception: ${e.getMessage}\n" +
+    s"stack trace:\n ${e.getStackTrace.mkString("\n")}"
+  
+  def forAll[A](as:Gen[A])(f:A=> Boolean):Prop = Prop {
+    (n,rng) => randomStream(as)(rng).zip(Stream.from(0)).take(n).map {
+      //a stream of pairs (a,i) where a is a random value and i is its index in the stream
+      case  (a,i)=> try{
+        if (f(a)) Passed else Falsified(a.toString, i)
+      } catch { 
+        //if an exception happens record it
+        case e:Exception => Falsified(buildMsg(a,e),i) }
+      }.find(_.isFalsified).getOrElse(Passed)
+    }
+  
 
 }
