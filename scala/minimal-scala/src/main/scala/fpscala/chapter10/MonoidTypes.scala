c7af9ce1bb2604335d71b96d2360ca5aa5a5dbd5
sajit <sajit.kunnumkal@gmail.com>
2016-07-28 23:18:48 -0400

    Ex 10.1

--- scala/minimal-scala/src/main/scala/fpscala/chapter10/MonoidTypes.scala
+++ scala/minimal-scala/src/main/scala/fpscala/chapter10/MonoidTypes.scala
@@ -5,4 +5,23 @@ object MonoidTypes {
     def op(a1:String,a2:String) = a1 + a2
     val zero = ""
   }
+  
+  val intAddition = new Monoid[Int] {
+    def op(a1:Int,a2:Int) = a1 + a2
+    val zero = 0
+  }
+  val intMultiplication = new Monoid[Int] {
+    def op(a1:Int,a2:Int) = a1 * a2
+    val zero = 1
+  }
+  
+  val booleanOr = new Monoid[Boolean] {
+    def op(a1:Boolean,a2:Boolean) = a1 || a2
+    val zero = false
+  }
+  val booleanAnd = new Monoid[Boolean] {
+    def op(a1:Boolean,a2:Boolean) = a1 && a2
+    val zero = true
+  }
+  
 }
