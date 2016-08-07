a450fd6c250984b53c171ae3e93d10443b05594a
sajit <sajit.kunnumkal@gmail.com>
2016-08-06 22:39:34 -0400

    chapter 10 tests

--- scala/minimal-scala/src/test/scala/fpscala/chapter10/Chapter10Spec.scala
+++ scala/minimal-scala/src/test/scala/fpscala/chapter10/Chapter10Spec.scala
@@ -1,7 +1,7 @@
 package fpscala.chapter10
 
 import fpscala.BaseSpec
-class Chapter10Spec extends BaseSpec{
+class Chapter10Spec extends BaseSpec{ 
   
   it should "behave like a monoid" in {
     MonoidTypes.stringMonoid.zero should be ("")
@@ -9,5 +9,19 @@ class Chapter10Spec extends BaseSpec{
     
 
   }
+  
+  it should "boolean Andify a Monoid" in {
+    MonoidTypes.booleanAnd.zero should be (true)
+    MonoidTypes.booleanAnd.op(true, false) should be (false)
+  }
+  
+  it should "identify function a Monoid" in {
+    val endMonoid:Monoid[Int => Int] = MonoidTypes.endoMonoid[Int]
+    endMonoid.zero(5) should be (5)
+    endMonoid.op({x => x + 1}, {x => x + 2})(1) should be (4)
+    endMonoid.op({x=> x + 2},{x => x + 1})(1) should be (4)
+  }
+
+  
   
 }
