26766236e92b5324dac781aa906018bb4e52bce7
sajit <sajit.kunnumkal@gmail.com>
2016-08-17 13:59:15 -0400

    Ex 10.13

--- scala/minimal-scala/src/main/scala/fpscala/chapter10/FoldableUtils.scala
+++ scala/minimal-scala/src/main/scala/fpscala/chapter10/FoldableUtils.scala
@@ -1,5 +1,7 @@
 package fpscala.chapter10
 
+import fpscala.chapter3._
+
 object FoldableUtils {
   
 }
@@ -25,4 +27,22 @@ class FoldableStream extends Foldable[Stream] {
   def foldMap[A, B](as: Stream[A])(f: A => B)(mn: Monoid[B]): B = as.foldRight(mn.zero)((a,b) => mn.op(b, f(a)))
 
   def foldRight[A, B](as: Stream[A])(z: B)(f: (A, B) => B): B = as.foldRight(z)(f)
+}
+
+class FoldableTree extends Foldable[CTree] {
+  def foldLeft[A, B](as: CTree[A])(z: B)(f: (B, A) => B): B = as match {
+    case Leaf(x) => z
+    case Branch(left,right) => {
+      foldLeft(right)(foldLeft(left)(z)(f))(f)
+    }
+  }
+
+  def foldMap[A, B](as: CTree[A])(f: A => B)(mn: Monoid[B]): B = foldRight(as)(mn.zero)((a,b) => mn.op(b,f(a)))
+
+  def foldRight[A, B](as: CTree[A])(z: B)(f: (A, B) => B): B = as match {
+   case Leaf(x) => z
+    case Branch(left,right) => {
+      foldRight(left)(foldRight(right)(z)(f))(f)
+    }
+  }
 }
