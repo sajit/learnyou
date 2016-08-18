e638eb6015e85be7b229908ad161ba2c9ac3f610
sajit <sajit.kunnumkal@gmail.com>
2016-08-18 12:28:04 -0400

    Ex 10.15

--- scala/minimal-scala/src/main/scala/fpscala/chapter10/Foldable.scala
+++ scala/minimal-scala/src/main/scala/fpscala/chapter10/Foldable.scala
@@ -5,4 +5,5 @@ trait Foldable[F[_]] {
   def foldLeft[A,B](as:F[A])(z:B)(f:(B,A) => B):B
   def foldMap[A,B](as:F[A])(f: A => B)(mn:Monoid[B]):B
   def concatenate[A](as:F[A])(m:Monoid[A]):A = foldLeft(as)(m.zero)(m.op)
+  def toList[A](fa:F[A]):List[A] = foldRight(fa)(List[A]())((a,soFar) => a :: soFar)
 }
