680a5d1796d2114cf1a6acea103e554e95b94da1
sajit <sajit.kunnumkal@gmail.com>
2016-08-18 12:15:14 -0400

    Ex 10.14

--- scala/minimal-scala/src/main/scala/fpscala/chapter10/FoldableUtils.scala
+++ scala/minimal-scala/src/main/scala/fpscala/chapter10/FoldableUtils.scala
@@ -46,3 +46,17 @@ class FoldableTree extends Foldable[CTree] {
     }
   }
 }
+
+class FoldableOption extends Foldable[Option] {
+  def foldLeft[A, B](as: Option[A])(z: B)(f: (B, A) => B): B = as match {
+    case Some(a) => f(z,a)
+    case None => z
+  }
+
+  def foldMap[A, B](as: Option[A])(f: A => B)(mn: Monoid[B]): B = foldRight(as)(mn.zero)((a,b) => mn.op(b,f(a)))
+
+  def foldRight[A, B](as: Option[A])(z: B)(f: (A, B) => B): B = as match {
+    case Some(a) => f(a,z)
+    case None => z
+  }
+}
