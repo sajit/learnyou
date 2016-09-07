b0db0c7e5fe09e96a5cd1e831a9846b0aad059e9
sajit <sajit.kunnumkal@gmail.com>
2016-09-07 15:43:17 -0400

    Intro.

--- scala/minimal-scala/src/main/scala/fpscala/chapter11/Monad.scala
+++ scala/minimal-scala/src/main/scala/fpscala/chapter11/Monad.scala
@@ -1,4 +1,5 @@
 package fpscala.chapter11
+import fpscala.chapter8.Gen
 
 trait Monad[F[_]] extends Functor[F]{
   def unit[A](a: => A):F[A]
@@ -8,4 +9,11 @@ trait Monad[F[_]] extends Functor[F]{
   
   def map2[A,B,C] (ma:F[A],mb:F[B])(f: (A,B) => C):F[C] = flatMap(ma)(a => map(mb)(b => f(a,b)))
   def map2_myv[A,B,C] (ma:F[A],mb:F[B])(f: (A,B) => C):F[C] = flatMap(ma)(a => flatMap(mb)(b => unit(f(a,b))))
+}
+
+object MonadUtils {
+  val genMonad = new Monad[Gen] {
+    def unit[A](a: => A):Gen[A]  = Gen.unit(a)
+    def flatMap[A,B](ma:Gen[A])(f: A => Gen[B]):Gen[B] = ma flatMap f 
+  }
 }
