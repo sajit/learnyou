67b4017227252e54965ead3a88b576bbf63c8e7a
sajit <sajit.kunnumkal@gmail.com>
2016-09-14 18:14:57 -0400

    Ex 12.2

--- scala/minimal-scala/src/main/scala/fpscala/chapter12/Applicative.scala
+++ scala/minimal-scala/src/main/scala/fpscala/chapter12/Applicative.scala
@@ -17,4 +17,13 @@ trait Applicative[F[_]] extends Functor[F] {
   def replicateM[A](n:Int,fa:F[A]):F[List[A]] = sequence(List.fill(n)(fa))
    
   def product[A,B](fa:F[A],fb:F[B]):F[(A,B)] = map2(fa,fb)((a,b) => (a,b))
+  
+  /**
+   * apply in terms of map2
+   */
+  def apply[A,B](fab:F[A => B])(fa:F[A]):F[B] = map2(fab,fa)((f,a) => f(a))
+  
+  def map2v2[A,B,C](fa:F[A],fb:F[B])(f:(A,B) => C):F[C] = ???
+  
+  def mapv2[A,B](fa:F[A])(f: A => B):F[B] = apply(unit(f))(fa)
 }
