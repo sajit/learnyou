970e13fd35c6714af97b81640f02dd948824adb8
sajit <sajit.kunnumkal@gmail.com>
2016-11-02 13:55:54 -0400

    Ex 13.1 JPHM

--- scala/minimal-scala/src/main/scala/fpscala/chapter13/Free.scala
+++ scala/minimal-scala/src/main/scala/fpscala/chapter13/Free.scala
@@ -1,6 +1,9 @@
 package fpscala.chapter13
 
-sealed trait Free[F[_],A]
-case class Return[F[_],A] (a: A) extends Free[F,A]
-case class Suspend[F[_],A](s:F[A]) extends Free[F,A]
-case class FlatMap[F[_],A,B](s: Free[F,A], f: A => Free[F,B]) extends Free[F,B]
+sealed trait Free[F[_],A] {
+  def flatMap[B] (f: A => Free[F,B]):Free[F,B] = FRFlatMap(this,f) //TRFlatMap(this,f)
+  def map[B](f: A => B):Free[F,B] = flatMap(f andThen( FRReturn(_)))
+}
+case class FRReturn[F[_],A] (a: A) extends Free[F,A]
+case class FRSuspend[F[_],A](s:F[A]) extends Free[F,A]
+case class FRFlatMap[F[_],A,B](s: Free[F,A], f: A => Free[F,B]) extends Free[F,B]
