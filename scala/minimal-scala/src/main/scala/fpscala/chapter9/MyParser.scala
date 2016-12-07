c0a1eb530a2dd5a2925ee21eef201767db111318
sajit <sajit.kunnumkal@gmail.com>
2016-12-07 15:19:51 -0500

    MyParser . wip1.

---
+++ scala/minimal-scala/src/main/scala/fpscala/chapter9/MyParser.scala
@@ -0,0 +1,39 @@
+package fpscala.chapter9
+
+
+
+
+case class Location(input: String, offset: Int = 0) {
+
+  lazy val line = input.slice(0,offset+1).count(_ == '\n') + 1
+  lazy val col = input.slice(0,offset+1).lastIndexOf('\n') match {
+    case -1 => offset + 1
+    case lineStart => offset - lineStart
+  }
+
+}
+
+case class ParseError(stack:List[(Location,String)]) {
+  def push(location:Location,message:String):ParseError = ParseError((location,message) :: stack)
+
+
+  def label[A](s:String):ParseError = ParseError((stack.lastOption map (_._1)).map((_,s)).toList)
+}
+
+sealed trait Result[+A]{
+
+  def extract: Either[ParseError,A] = this match {
+    case Failure(e) => Left(e)
+    case Success(v,_) => Right(v)
+  }
+}
+case class Success[+A](get:A,charsConsumed:Int) extends Result[A]
+case class Failure(get:ParseError) extends Result[Nothing]
+
+/** A parser is a kind of state action that can fail. */
+object Utils{
+  type Parser[+A] = Location => Result[A]
+}
+
+
+
